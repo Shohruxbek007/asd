0675e33afc5fe2483f77b1fd3b434d3a38c24c88
Shohruxbek007 <asildinovofficial@gmail.com>
2025-09-10 15:17:02 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+let chat = document.getElementById("chat")
+let box = document.getElementById("box")
+let Chat = document.querySelector(".chat")
+
+function send() {
+    let inp = document.getElementById("inp")
+    if (box.checked) {
+        Chat.innerHTML = Chat.innerHTML + "<div class='right'><h3 align='right'>" + inp.value + "</h3></div>"
+        // console.log("ong")
+    }
+    else {
+        Chat.innerHTML = Chat.innerHTML + "<div class='left'><h3 align='left'>" + inp.value + "</h3></div>"
+        // console.log("chap")
+    }
+
+
+    inp.focus()
+    inp.value = ''
+    event.preventDefault()
+}
+
+
+function check() 
+{
+    let inp = document.getElementById("inp")
+    if (inp.value == "") {
+        console.log("error")
+    }
+
+    else {
+        send()
+    }
+}
 
